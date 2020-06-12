07bb1c5105bc8fca5b4c852f99d0fcbf79c40e64
pratima-ux <pratima7218@gmail.com>
2020-06-12 20:09:16 +0100

    javafirsthw

---
+++ src/MyDetail.java
@@ -0,0 +1,47 @@
+//please find my personal Detail
+
+public class MyDetail {
+    public static void main (String [] args){
+        String name = "Name:- Mrs Pratima Patel";
+        //I have put my full name
+
+        String address = "  1 High Road";
+        //I have put my address first line
+
+        String mobileno = "Mobile no:  07836123456";
+        //I have put my mobole no
+
+        int age = 35;
+        //I have put my age
+
+        int salary = 25000;
+        //I have put my annual salary
+        String email = "pratima7218@gmail.com";
+
+        System.out.println(name);
+        //I want to print out my name
+        System.out.print("Address:");
+
+        System.out.println(address);
+        //I want to print my address
+        System.out.println("          Wembley");
+        System.out.println("          HA02JJ");
+
+        System.out.println(mobileno);
+        // I want to print my mobile no
+
+        System.out.print("Age:  ");
+        System.out.println(age);
+        // I want to print out my age
+
+        System.out.print("Salary: ");
+        System.out.println(salary);
+        // I want to print my salary
+
+        System.out.print("E-mail: ");
+        System.out.println(email);
+        // I want to print out my e-mail address
+    }
+
+}
+
